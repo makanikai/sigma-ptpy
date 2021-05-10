7f450115484a9eec328d5de50adbd4181b40e98d
Akinori Abe <abe_akinori@openlogi.com>
2021-05-10 17:16:35 +0900

    :art: ignore C901 (too complex)

--- git/pre-commit.sh
+++ git/pre-commit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 flake8 sigma_ptpy examples tests --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 sigma_ptpy examples tests --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+flake8 sigma_ptpy examples tests --count --exit-zero --ignore=C901 --max-line-length=127 --statistics
